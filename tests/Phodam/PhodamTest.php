8a66681a7bf22286210019d38880e39191a6f897
Andrew Vehlies <avehlies@gmail.com>
2022-05-28 17:30:50 -0400

    Add license header

--- tests/Phodam/PhodamTest.php
+++ tests/Phodam/PhodamTest.php
@@ -1,5 +1,10 @@
 <?php
 
+// This file is part of Phodam
+// Copyright (c) Andrew Vehlies <avehlies@gmail.com>
+// Licensed under the MIT license. See LICENSE file in the project root.
+// SPDX-License-Identifier: MIT
+
 declare(strict_types=1);
 
 namespace Phodam\Tests\Phodam;
