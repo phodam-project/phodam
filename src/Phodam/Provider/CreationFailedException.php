4c8abbd1e593173dd2048ff7c916b44e964364f8
Chris Bouchard <chris@upliftinglemma.net>
2022-06-14 23:28:10 -0400

    Create a new exception type for "creation failed"
    The idea is that Phodam will throw this exception type, wrapping any exception
    that was thrown by the provider. This will let callers handle a single known
    exception type, and will let providers can throw various more meaningful
    exception types.

---
+++ src/Phodam/Provider/CreationFailedException.php
@@ -0,0 +1,66 @@
+<?php
+
+// This file is part of Phodam
+// Copyright (c) Andrew Vehlies <avehlies@gmail.com>
+// Licensed under the MIT license. See LICENSE file in the project root.
+// SPDX-License-Identifier: MIT
+
+declare(strict_types=1);
+
+namespace Phodam\Provider;
+
+use Phodam\PhodamException;
+use Throwable;
+
+class CreationFailedException extends PhodamException
+{
+    private string $type;
+    private ?string $name;
+
+    /**
+     * @param string $type
+     * @param ?string $name
+     * @param ?string $message
+     * @param ?Throwable $previous
+     */
+    public function __construct(
+        string $type,
+        ?string $name,
+        ?string $message = null,
+        ?Throwable $previous = null
+    ) {
+        $message = $message ?? self::defaultMessage($type, $name);
+
+        parent::__construct($message, 0, $previous);
+
+        $this->type = $type;
+        $this->name = $name;
+    }
+
+    private static function defaultMessage(string $type, ?string $name): string
+    {
+        if ($name !== null) {
+            $providerClause = "provider named {$name}";
+        } else {
+            $providerClause = "default provider";
+        }
+
+        return "Creation failed for type {$type} using {$providerClause}";
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return ?string
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+}
---
+++ tests/Phodam/Provider/CreationFailedExceptionTest.php
@@ -0,0 +1,109 @@
+<?php
+
+// This file is part of Phodam
+// Copyright (c) Andrew Vehlies <avehlies@gmail.com>
+// Licensed under the MIT license. See LICENSE file in the project root.
+// SPDX-License-Identifier: MIT
+
+declare(strict_types=1);
+
+namespace Phodam\Tests\Phodam\Provider;
+
+use Exception;
+use Phodam\Provider\CreationFailedException;
+use Phodam\Tests\Fixtures\SimpleType;
+use Phodam\Tests\Phodam\PhodamBaseTestCase;
+use Throwable;
+
+/**
+ * @coversDefaultClass \Phodam\Provider\CreationFailedException
+ */
+class CreationFailedExceptionTest extends PhodamBaseTestCase
+{
+    public function provideConstructorArgs()
+    {
+        return [
+            'type, no name, no message, no previous' => [
+                'type' => SimpleType::class,
+                'name' => null,
+                'message' => null,
+                'expectedMessage' => (
+                    'Creation failed for type Phodam\Tests\Fixtures\SimpleType using default provider'
+                ),
+                'previous' => null,
+            ],
+            'type, name, no message, no previous' => [
+                'type' => SimpleType::class,
+                'name' => 'MyProviderName',
+                'message' => null,
+                'expectedMessage' => (
+                    'Creation failed for type Phodam\Tests\Fixtures\SimpleType using provider named MyProviderName'
+                ),
+                'previous' => null,
+            ],
+            'type, name, message, no previous' => [
+                'type' => SimpleType::class,
+                'name' => 'MyProviderName',
+                'message' => 'My message here',
+                'expectedMessage' => 'My message here',
+                'previous' => null,
+            ],
+            'type, no name, no message, previous' => [
+                'type' => SimpleType::class,
+                'name' => null,
+                'message' => null,
+                'expectedMessage' => (
+                    'Creation failed for type Phodam\Tests\Fixtures\SimpleType using default provider'
+                ),
+                'previous' => new Exception(),
+            ],
+            'type, name, no message, previous' => [
+                'type' => SimpleType::class,
+                'name' => 'MyProviderName',
+                'message' => null,
+                'expectedMessage' => (
+                    'Creation failed for type Phodam\Tests\Fixtures\SimpleType using provider named MyProviderName'
+                ),
+                'previous' => new Exception(),
+            ],
+            'type, name, message, previous' => [
+                'type' => SimpleType::class,
+                'name' => 'MyProviderName',
+                'message' => 'My message here',
+                'expectedMessage' => 'My message here',
+                'previous' => new Exception(),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideConstructorArgs
+     * @covers ::__construct
+     * @covers ::getType
+     */
+    public function testConstruct(
+        string $type,
+        ?string $name,
+        ?string $message,
+        string $expectedMessage,
+        ?Throwable $previous
+    ): void {
+        $ex = new CreationFailedException(
+            $type,
+            $name,
+            $message,
+            $previous
+        );
+
+        // These should all be forwarded exactly.
+        $this->assertSame($type, $ex->getType());
+        $this->assertSame($name, $ex->getName());
+        $this->assertSame($previous, $ex->getPrevious());
+
+        // Message has a default when $message === null.
+        $this->assertSame($expectedMessage, $ex->getMessage());
+
+        // Code is not passed, but should always be 0.
+        $this->assertSame(0, $ex->getCode());
+    }
+}
