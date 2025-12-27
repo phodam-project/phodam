9e0a78a952b0ab2126ed22b4d7c688665bdc43c2
Andrew Vehlies <avehlies@gmail.com>
2025-12-27 09:43:36 -0500

    Ex03_NamedProviders

---
+++ examples/Ex03_NamedProviders/ActiveUserProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+// This file is part of Phodam
+// Copyright (c) Andrew Vehlies <avehlies@gmail.com>
+// Licensed under the MIT license. See LICENSE file in the project root.
+// SPDX-License-Identifier: MIT
+
+declare(strict_types=1);
+
+namespace PhodamExamples\Ex03_NamedProviders;
+
+use Phodam\Provider\ProviderContext;
+use Phodam\Provider\TypedProviderInterface;
+
+/**
+ * @template T extends User
+ * @template-implements TypedProviderInterface<User>
+ */
+class ActiveUserProvider implements TypedProviderInterface
+{
+    /**
+     * @inheritDoc
+     * @return User
+     */
+    public function create(ProviderContext $context): User
+    {
+        $defaults = [
+            'name' => $context->create('string'),
+            'email' => $context->create('string'),
+            'active' => true  // Always active
+        ];
+
+        $values = array_merge(
+            $defaults,
+            $context->getOverrides()
+        );
+
+        return (new User())
+            ->setName($values['name'])
+            ->setEmail($values['email'])
+            ->setActive((bool) $values['active']);
+    }
+}
+
