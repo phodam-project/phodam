a4f4b1e200e35a4acb09f0be897e68c3ed65c94b
Andrew Vehlies <avehlies@gmail.com>
2026-01-03 11:19:11 -0500

    Update copyrights

--- tests/Fixtures/TestNamedArrayProviderWithAttribute.php
+++ tests/Fixtures/TestNamedArrayProviderWithAttribute.php
@@ -1,6 +1,7 @@
 <?php
 
 // This file is part of Phodam
+// Copyright (c) Andrew Vehlies <avehlies@gmail.com>
 // Copyright (c) Chris Bouchard <chris@upliftinglemma.net>
 // Licensed under the MIT license. See LICENSE file in the project root.
 // SPDX-License-Identifier: MIT
